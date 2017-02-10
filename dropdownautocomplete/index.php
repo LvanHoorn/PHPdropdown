748180decac109915b8017a8ed2aa50a43d1455a
Luciano van Hoorn <luciano@MacBook-Pro-van-Luciano.local>
2017-02-10 13:25:01 +0100

    third commit

--- dropdownautocomplete/index.php
+++ dropdownautocomplete/index.php
@@ -7,9 +7,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>User search</title>
+    
+    <link rel="stylesheet" href="css/global.css">
+    
 </head>
 <body>
     
+    <form action="index.php" method="get" autocomplete="on">
+        <input type="text" name="user" id="users">
+        <input type="submit" value="Go">    
+    </form>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/typehead.js"></script>
+    <script src="js/global.js"></script>
 </body>
 </html>
---
+++ dropdownautocomplete/js/global.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var users = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('username'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: 'users.php?query=%QUERY'
+    });
+    
+    users.initialize();
+    
+    $('#users').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 2
+    }, {
+        name: 'users',
+        displayKey: 'username',
+        source: users.ttAdapter()
+    });
+});
